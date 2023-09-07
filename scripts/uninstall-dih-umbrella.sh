ce6cef993dd149af380a38fbec680a7b6d4d6b34
Alon Segal <segalalon@gmail.com>
2023-09-07 16:45:52 +0300

    added checks for uninstall

--- scripts/uninstall-dih-umbrella.sh
+++ scripts/uninstall-dih-umbrella.sh
@@ -10,17 +10,41 @@ case "${response,,}" in
 esac
 
 # delete feeder
-helm uninstall space-feeder
+SVC="space-feeder"
+if [[ $(helm ls | grep $SVC | wc -l) -eq 1 ]]; then
+    helm uninstall $SVC
+else
+    echo "no deployment for '${SVC}' found. nothing to do"
+fi
 
 # delete space
-helm uninstall space
+SVC="space"
+if [[ $(helm ls | grep $SVC | wc -l) -eq 1 ]]; then
+    helm uninstall $SVC
+else
+    echo "no deployment for '${SVC}' found. nothing to do"
+fi
+
 
 # delete dih
-helm uninstall dih
+SVC="dih"
+if [[ $(helm ls | grep $SVC | wc -l) -eq 1 ]]; then
+    helm uninstall $SVC
+else
+    echo "no deployment for '${SVC}' found. nothing to do"
+fi
 
 # delete ingress controller
-$uninstall_ingress && helm uninstall ingress-nginx
+$uninstall_ingress && {
+    SVC="ingress-nginx"
+    if [[ $(helm ls | grep $SVC | wc -l) -eq 1 ]]; then
+        helm uninstall $SVC
+    else
+        echo "no deployment for '${SVC}' found. nothing to do"
+    fi
+}
 
+# poll kubectl to verify all pods are gone
 while true; do
     num_pods=$(kubectl get pods -o name | grep -v ingress-nginx | wc -l)
     if [[ $num_pods -eq 0 ]]; then
