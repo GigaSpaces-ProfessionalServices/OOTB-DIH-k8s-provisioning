5427f5f21dddbdbb03236e57ee9276eb66292126
Alon Segal <segalalon@gmail.com>
2023-09-18 13:38:34 +0300

    added test cluster exists

--- scripts/destroy-eks-lab.sh
+++ scripts/destroy-eks-lab.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# destry eks cluster
+# destroy eks cluster
 
 PARENT_DIR=$(realpath "$(dirname $(realpath $0))/..")
 SCRIPTS="${PARENT_DIR}/scripts"
-
+CLUSTER_NAME=$(cat $PARENT_DIR/clusterName.txt)
 source ${PARENT_DIR}/setAWSEnv.sh
+
+if [[ $(aws eks list-clusters | grep -q $CLUSTER_NAME ; echo $?) -ne 0 ]]; then
+    echo -e "\nEKS cluster '$CLUSTER_NAME' does not exist. nothing to destroy.\n"
+    exit
+fi
 
 while true; do
     echo ; read -r -p "Do you want to destroy your EKS lab ?[yes/no] " response
